5c90369855e0df018e80adbeb8409c576ba2a03a
122320730@qq.com <122320730@qq.com>
2019-06-20 16:29:10 +0800

    引入 sentinel 模块

--- config-client/src/main/java/com/xun/config/ConfigClientApplication.java
+++ config-client/src/main/java/com/xun/config/ConfigClientApplication.java
@@ -3,6 +3,9 @@ package com.xun.config;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author Uny
+ */
 @SpringBootApplication
 public class ConfigClientApplication {
 
